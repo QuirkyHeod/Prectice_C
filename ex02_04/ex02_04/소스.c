fee0f4497df3066d5e09f28b240c0382dfaf62c0
[QuirkyHoed] <[dydwo3239@gmail.com]>
2025-09-11 17:24:06 +0900

    프로젝트 폴더명 변경 및 .gitignore 적용

---
+++ ex02_04/ex02_04/소스.c
@@ -0,0 +1,8 @@
+/* 문법 오류 세 번째 프로그램 */
+#include <stdio.h>
+
+int main(void)
+{
+	print("문법 오류: 세 번째 테스트\n"); //printf()를 사용해야함
+	return 0;
+}
---
+++ ex02_05/ex02_05/소스.c
@@ -0,0 +1,9 @@
+/* 논리 오류 프로그램 */
+#include <stdio.h>
+
+int main(void)
+{
+	printf("논리 오류 발생");  // \n을 선언해야 2행으로 출력는 상황에서 \n을 작성하지 않음
+	printf("소스 코드 확인");
+	return 0;
+}
